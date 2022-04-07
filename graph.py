b3bb9201d902c5fe1404f8b61d6e589468abf5eb
Alex Kuczala <kookzala@gmail.com>
2022-04-07 19:10:08 -0400

    use fixed adt package, add mypy.ini

--- graph.py
+++ graph.py
@@ -1,11 +1,15 @@
-from __future__ import annotations
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 from dataclasses import dataclass
-from typing import TypeVar, Callable, Union, Type, Dict, cast
+from functools import partial
+from typing import TypeVar, Callable, Union, Type, Dict, cast, Optional
 
-from adt import ADT, ValueType
+from adt import adt, Case
 
 V = TypeVar('V', bound="GraphElementValue")
+
+
+class ValueType(ABC):
+    pass
 
 
 class GraphElementValue(ValueType):
@@ -30,7 +34,7 @@ class Constant(GraphElementValue):
             print(f"encountered constant {self.value}. Should be done.")
         stack.pop()
 
-    def equals_literal(self, other: Constant) -> bool:
+    def equals_literal(self, other: "Constant") -> bool:
         return self.value == other.value
 
 
@@ -52,7 +56,7 @@ class Combinator(GraphElementValue):
             for _ in range(self.n_args):
                 stack.pop()
 
-    def equals_literal(self, other: Combinator) -> bool:
+    def equals_literal(self, other: "Combinator") -> bool:
         return type(self) == type(other)
 
     @abstractmethod
@@ -70,11 +74,11 @@ class Combinator(GraphElementValue):
 
 @dataclass(frozen=True)
 class Node(GraphElementValue):
-    function_slot: GraphElement
-    argument_slot: GraphElement
+    function_slot: "GraphElement"
+    argument_slot: "GraphElement"
 
     @classmethod
-    def new(cls, in_function_slot, in_argument_slot) -> Node:
+    def new(cls, in_function_slot, in_argument_slot) -> "Node":
         return cls(
             function_slot=GraphElement.new(in_function_slot),
             argument_slot=GraphElement.new(in_argument_slot)
@@ -83,7 +87,7 @@ class Node(GraphElementValue):
     def eval_and_update_stack(self, stack):
         stack.push(self.function_slot)
 
-    def equals_literal(self, other: Node) -> bool:
+    def equals_literal(self, other: "Node") -> bool:
         return (
                 self.function_slot.equals_literal(other.function_slot) and
                 self.argument_slot.equals_literal(other.argument_slot)
@@ -96,43 +100,43 @@ class Node(GraphElementValue):
 T = TypeVar('T')
 
 
-@dataclass
-class GraphElement(ADT):
-    value: GraphElementValue
+# todo: figure out how references work here. Might want to make GraphElementValue an ADT instead
+# we want to be able to "swap out" different ADT instances at the same place in memory
+@adt
+class GraphElement:
+    CONSTANT: Case[Constant]
+    COMBINATOR: Case[Combinator]
+    NODE: Case[Node]
 
     @classmethod
-    def new(cls, in_slot: Union[GraphElement, GraphElementValue, Type[Combinator], float, int]) -> GraphElement:
+    def new(cls, in_slot: Union["GraphElement", GraphElementValue, Type[Combinator], float, int]) -> "GraphElement":
         """
         Convenience function. singledispatchmethod doesn't quite cut it, so here we are
         """
         match in_slot:
             case s if isinstance(s, GraphElement):
                 return cast(GraphElement, in_slot)
-            case s if isinstance(s, GraphElementValue):
-                return cls(s)
+            case s if isinstance(s, Constant):
+                return cls.CONSTANT(s)
+            case s if isinstance(s, Combinator):
+                return cls.COMBINATOR(s)
+            case s if isinstance(s, Node):
+                return cls.NODE(s)
             case s if isinstance(s, type(Combinator)):
-                return cls(in_slot())  # type: ignore
+                return cls.COMBINATOR(in_slot())  # type: ignore
             case s if isinstance(s, float):
-                return cls(Constant(s))
+                return cls.CONSTANT(Constant(s))
             case s if isinstance(s, int):
-                return cls(Constant(float(s)))
+                return cls.CONSTANT(Constant(float(s)))
         # had to take this out of case _: to make mypy happy
         raise NotImplementedError(f"Cannot instantiate GraphElement with {in_slot} of type {type(in_slot)}.")
 
     @classmethod
-    def new_node(cls, function_slot: GraphElement, argument_slot: GraphElement) -> GraphElement:
-        return cls.new(Node.new(function_slot, argument_slot))
+    def new_node(cls, function_slot: "GraphElement", argument_slot: "GraphElement") -> "GraphElement":
+        return cls.NODE(Node.new(function_slot, argument_slot))
 
     def eval_and_update_stack(self, stack):
         self.value.eval_and_update_stack(stack)
-
-    @property
-    def match_kwargs(self) -> Dict[Type[V], str]:
-        return {
-            Node: 'on_node',
-            Combinator: 'on_combinator',
-            Constant: 'on_constant'
-        }
 
     @property
     def _default_expect_value_exceptions(self) -> Dict[str, Callable[[ValueType], Exception]]:
@@ -142,10 +146,37 @@ class GraphElement(ADT):
             on_constant=lambda c: ValueError(f"Did not expect constant {c}")
         )
 
-    def equals_literal(self, other: GraphElement) -> bool:
-        if not isinstance(self.value, type(other.value)):
-            return False
-        return self.value.equals_literal(other.value)
+    def equals_literal(self, other: "GraphElement") -> bool:
+        eq = lambda s1, s2: s1.equals_literal(s2)
+        eqs: Callable[[GraphElementValue], bool] = lambda s: other.match(
+            constant=partial(eq)(s), combinator=partial(eq)(s), node=partial(eq)(s)
+        )
+        return self.match(
+            constant=eqs, combinator=eqs, node=eqs
+        )
 
     def __str__(self) -> str:
-        return str(self.value)
+        return self.match(
+            constant=str,
+            combinator=str,
+            node=str
+        )
+
+    def match_value(self, on_constant, on_combinator, on_node) -> T:
+        return self.match(
+            constant=on_constant,
+            combinator=on_combinator,
+            node=on_node
+        )
+
+    def expect_value(self,
+                     **kwargs: Callable[[ValueType], Union[T, Exception]]) -> T:
+        """
+        Raises error if we match on something we don't specify a lambda for. Error messages can be customized
+        by passing lambdas that return Exceptions
+        """
+        result: T = self.match_value(**dict(self._default_expect_value_exceptions, **kwargs))
+        if isinstance(result, Exception):
+            raise result
+        else:
+            return result
